b82705f1294b90a03d88ff1b476532898f238abc
Insortial <renwell2099@gmail.com>
2023-08-15 13:15:25 -0700

    Made changes to CORS

--- my-app/server.js
+++ my-app/server.js
@@ -1,16 +1,24 @@
-const socketIO = require("socket.io");
-const app = require('express');
-const http = require('http').createServer(app)
-const io = new socketIO.Server(http, {
-  cors: {
-    origin: "http://localhost:3001"
-  }
+const express = require('express');
+const favicon = require('express-favicon');
+const http = require('http');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+const server = http.createServer(app);
+const io = module.exports.io = require('socket.io')(server)
+app.use(favicon(__dirname + '/build/favicon.ico'));
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-http.listen(4000, function() {
-  console.log('listening on port 4000')
-})
+server.listen(port);
 
+//Socket.io code
 io.on('connection', socket => {
   console.log('User connected');
 
--- my-app/src/Components/SocketProvider.js
+++ my-app/src/Components/SocketProvider.js
@@ -1,5 +1,14 @@
 import React, {useContext, useEffect, useState} from 'react'
-import io from 'socket.io-client'
+import { io } from "socket.io-client";
+
+
+
+const socket = io("https://api.example.com", {
+  withCredentials: true,
+  extraHeaders: {
+    "my-custom-header": "abcd"
+  }
+});
 
 
 const SocketContext = React.createContext()
@@ -12,7 +21,8 @@ export function SocketProvider({ children }) {
     const [socket, setSocket] = useState()
 
     useEffect(() => {
-        const newSocket = io('http://localhost:4000');
+        //const newSocket = io('https://renwell.dev/api');
+        const newSocket = io('http://localhost:400/');
         setSocket(newSocket)
         return () => newSocket.close()
     }, [])
